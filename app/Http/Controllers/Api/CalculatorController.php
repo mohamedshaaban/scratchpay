9b6c95d41c3533a9fe30e678d1bdd0c5789f8bb0
mohamed shaban <mohammad.shabaan@gmail.com>
2019-06-08 15:45:11 +0300

    inital

---
+++ app/Http/Controllers/Api/CalculatorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+class CalculatorController extends Controller
+{
+    public function show(Request $request)
+    {
+        dd('123');
+    }
+}
